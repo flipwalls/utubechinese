7546b8564d60efecbe71937fddf09e227d68847a
flipwalls <1134549391@qq.com>
2019-06-04 13:21:11 +0800

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@
     // of different video formats to add
     src: [
       {
-        src: 'right.mp4',
+        src: 'https://www.zisu.design/down/%E9%9F%B3%E4%B9%90/MV/right.mp4',
         type: 'video/mp4'
       },
       {
